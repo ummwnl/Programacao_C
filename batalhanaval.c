80737fddde3164a2549be1530ace973d3ad6fabf
ummwnl <uummwwnnll@gmail.com>
2025-05-01 19:38:30 +0000

    Update batalhanaval

--- batalhanaval.c
+++ batalhanaval.c
@@ -1,47 +1,79 @@
 #include <stdio.h>
 
-#define COLUNA 10
-#define LINHA 10
+#define COLUNA 9
+#define LINHA 9
 
-int main(){
-    int cabecalho_linha[LINHA] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    char cabecalho_coluna[COLUNA] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int tabuleiro[LINHA][COLUNA] = {0};
+int tabuleiro[LINHA][COLUNA] = {0};
+int x, y;
 
-    tabuleiro[0][1] = 3;
-    tabuleiro[0][2] = 3;
+void TABULEIRO(){
+    int cabecalho_linha[LINHA] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    char cabecalho_coluna[COLUNA] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I'};
 
-    tabuleiro[7][7] = 3;
-    tabuleiro[6][7] = 3;
-    tabuleiro[5][7] = 3;
+    printf("  --BATALHA NAVAL--\n ");
 
-    tabuleiro[3][4] = 3;
-    tabuleiro[4][3] = 3;
-
-    int i, j;
-
-    for (i = 0, j = 10; i < 10 && j > 0; i++, j--){
-        tabuleiro[i][i] = 3;
-        tabuleiro[j][i] = 3;
-    }
-
-    printf("  --BATALHA NAVAL--\n");
-
-    printf(" ");
-
-    int x, y;
-
-    for(y = 0; y < 10; y++){
+    for(y = 0; y < COLUNA; y++){
         printf(" %c", cabecalho_coluna[y]);
     }
 
-    for (x = 0; x < 10; x++){
+    for (x = 0; x < LINHA; x++){
         printf("\n%d ", cabecalho_linha[x]);
-        for (y = 0; y < 10; y++){
+        for (y = 0; y < COLUNA; y++){
             printf("%d ", tabuleiro[x][y]);
-            }
+        }
     }
-    
 
+    printf("\n\n");
+}
+
+int main(){
+    int escolha = 0;
+
+    TABULEIRO();
+
+    printf("Digite 1 para pintar as bordas\n");
+    printf("Digite 2 para pintar duas casas diagonais, verticais e horizontais\n");
+    printf("Digite 3 para pintar um x no centro\n");
+    printf("Digite qualquer coisa para sair do programa\n");
+    scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+        case 1:
+            tabuleiro[0][0] = 3;
+            tabuleiro[8][8] = 3;
+            tabuleiro[8][0] = 3;
+            tabuleiro[0][8] = 3;
+
+            TABULEIRO();
+        break;
+
+        case 2:
+            tabuleiro[1][1] = 3;
+            tabuleiro[1][2] = 3;
+
+            tabuleiro[7][7] = 3;
+            tabuleiro[6][7] = 3;
+
+            tabuleiro[3][4] = 3;
+            tabuleiro[4][3] = 3;
+
+            TABULEIRO();
+        break;
+
+        case 3:
+            for (x = 0, y = COLUNA -1; x <= LINHA && y >= 0; x++, y--){
+                tabuleiro[x][x] = 3;
+                tabuleiro[x][y] = 3;
+            }
+        
+        TABULEIRO();
+        break;
+        
+        default:
+            printf("Saindo...\n");
+        break;
+    }
+   
     return 0;
 }
